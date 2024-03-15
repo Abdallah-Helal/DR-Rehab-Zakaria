3dd8a0b696dfdff5ada130ab64a836da2749a95f
Abdallah-Helal <abdallahelal102@gmail.com>
2024-03-15 03:22:31 +0200

    add service

--- pages/service.js
+++ pages/service.js
@@ -1,3 +1,23 @@
+import styles from "../styles/service.module.css";
+
 export default function Service() {
-  return <h1>Service</h1>;
+  return (
+    <section className={styles.section1}>
+      <img src="../Img/cover.jpg" alt="image" />
+      <div></div>
+      <h2 className={styles["special-heading"]}>Services</h2>
+      <p>We Provide The Following Services</p>
+
+      <ul>
+        <li>Botox</li>
+        <li>Skin Boosters</li>
+        <li>Dermapen</li>
+        <li>Cold Peel</li>
+        <li>Plasmage</li>
+        <li>Fractional Laser</li>
+        <li>Fillers</li>
+      </ul>
+      <div></div>
+    </section>
+  );
 }
