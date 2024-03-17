71f5eeff5b2586df0ced6b7f9812c7e9f6580100
Abdallah-Helal <abdallahelal102@gmail.com>
2024-03-17 20:10:56 +0200

    add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
 import MainLayout from "../components/MainLayout";
 import "../styles/global.css";
 
+import { useEffect } from "react";
+import { initGA, logPageView } from "./analytics";
+
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    if (!window.GA_INITIALIZED) {
+      initGA();
+      window.GA_INITIALIZED = true;
+    }
+    logPageView();
+  }, []);
+
   return (
     <MainLayout>
       <Component {...pageProps} />
