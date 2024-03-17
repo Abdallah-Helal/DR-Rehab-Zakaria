f952b2a1134affb1a84d646a113b1d4ab270a3ad
Abdallah-Helal <abdallahelal102@gmail.com>
2024-03-17 03:08:07 +0200

    add map

--- pages/contact.js
+++ pages/contact.js
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import styles from "../styles/contact.module.css";
 import { Facebook, Phone, Instagram } from "lucide-react";
+import dynamic from "next/dynamic";
+import OpenStreetMap from "../components/OpenStreetMap";
 
 const services = [
   "Botox",
@@ -41,93 +43,97 @@ export default function Contact() {
   };
 
   return (
-    <div className={styles.contact}>
-      <div className={styles.address}>
-        <p className={styles.heading}>Where to Find Us</p>
-        <p>
-          Trivium Business Complex, near FUE, floors one through five Settlement
-        </p>
-        <a href="tel:01064022402" target="_blank" className={styles.phone}>
-          <Phone className={styles.lucide} /> {"  "} 01064022402
-        </a>
+    <>
+      <OpenStreetMap />
+      <div className={styles.contact}>
+        <div className={styles.address}>
+          <p className={styles.heading}>Where to Find Us</p>
+          <p>
+            Trivium Business Complex, near FUE, floors one through five
+            Settlement
+          </p>
+          <a href="tel:01064022402" target="_blank" className={styles.phone}>
+            <Phone className={styles.lucide} /> {"  "} 01064022402
+          </a>
+        </div>
+        <div className={styles.formsection}>
+          <p className={styles.specialHeading}>Reservation</p>
+          <form className={styles.formContainer} onSubmit={handleSubmit}>
+            <label className={styles.label}>
+              Name:
+              <input
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                className={styles.input}
+                required
+              />
+            </label>
+            <label className={styles.label}>
+              Email:
+              <input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className={styles.input}
+                required
+              />
+            </label>
+            <label className={styles.label}>
+              Phone:
+              <input
+                type="tel"
+                value={phone}
+                onChange={(e) => setPhone(e.target.value)}
+                className={styles.input}
+                required
+              />
+            </label>
+            <label className={styles.label}>
+              Date:
+              <input
+                type="date"
+                value={date}
+                onChange={(e) => setDate(e.target.value)}
+                className={styles.input}
+                required
+              />
+            </label>
+            <label className={styles.label}>
+              Time Slot:
+              <select
+                value={timeSlot}
+                onChange={(e) => setTimeSlot(e.target.value)}
+                className={styles.select}
+                required
+              >
+                <option value="">Select a time slot</option>
+                <option value="morning">Morning</option>
+                <option value="evening">Evening</option>
+              </select>
+            </label>
+            <label className={styles.label}>
+              Select Service:
+              <select
+                value={selectedService}
+                onChange={(e) => setSelectedService(e.target.value)}
+                className={styles.select}
+                required
+              >
+                <option value="">Select a service</option>
+                {services.map((service) => (
+                  <option key={service} value={service}>
+                    {service}
+                  </option>
+                ))}
+              </select>
+            </label>
+            <button type="submit" className={styles.button}>
+              Submit
+            </button>
+          </form>
+        </div>
       </div>
-      <div className={styles.formsection}>
-        <p className={styles.specialHeading}>Reservation</p>
-        <form className={styles.formContainer} onSubmit={handleSubmit}>
-          <label className={styles.label}>
-            Name:
-            <input
-              type="text"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              className={styles.input}
-              required
-            />
-          </label>
-          <label className={styles.label}>
-            Email:
-            <input
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className={styles.input}
-              required
-            />
-          </label>
-          <label className={styles.label}>
-            Phone:
-            <input
-              type="tel"
-              value={phone}
-              onChange={(e) => setPhone(e.target.value)}
-              className={styles.input}
-              required
-            />
-          </label>
-          <label className={styles.label}>
-            Date:
-            <input
-              type="date"
-              value={date}
-              onChange={(e) => setDate(e.target.value)}
-              className={styles.input}
-              required
-            />
-          </label>
-          <label className={styles.label}>
-            Time Slot:
-            <select
-              value={timeSlot}
-              onChange={(e) => setTimeSlot(e.target.value)}
-              className={styles.select}
-              required
-            >
-              <option value="">Select a time slot</option>
-              <option value="morning">Morning</option>
-              <option value="evening">Evening</option>
-            </select>
-          </label>
-          <label className={styles.label}>
-            Select Service:
-            <select
-              value={selectedService}
-              onChange={(e) => setSelectedService(e.target.value)}
-              className={styles.select}
-              required
-            >
-              <option value="">Select a service</option>
-              {services.map((service) => (
-                <option key={service} value={service}>
-                  {service}
-                </option>
-              ))}
-            </select>
-          </label>
-          <button type="submit" className={styles.button}>
-            Submit
-          </button>
-        </form>
-      </div>
-    </div>
+    </>
   );
 }
