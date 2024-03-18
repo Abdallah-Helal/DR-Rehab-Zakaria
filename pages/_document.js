7cf2d5d9c59751003e043c45a42b0c41b117a92b
Abdallah-Helal <abdallahelal102@gmail.com>
2024-03-18 02:58:40 +0200

    edit contact form

--- pages/_document.js
+++ pages/_document.js
@@ -6,16 +6,6 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <title>Dr - Rehab Zakria</title>
-          <meta name="description" content="DR Rehab Zakaria WebSite" />
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1.0"
-          />
-
-          <link rel="icon" sizes="180x180" href="/Img/favicon-180x180.png" />
-          <link rel="icon" sizes="32x32" href="/Img/favicon-32x32.png" />
-          <link rel="icon" sizes="16x16" href="/Img/favicon-16x16.png" />
           {/* <script>
             (function(w,d,s,l,i){
                 w[l]=w[l]||[];
