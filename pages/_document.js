877f540d55500646a57acde3094a05892f022799
Abdallah-Helal <abdallahelal102@gmail.com>
2024-03-18 03:04:59 +0200

    add lang

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,7 @@ import Document, { Html, Head, Main, NextScript } from "next/document";
 class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang="en">
         <Head>
           {/* <script>
             (function(w,d,s,l,i){
