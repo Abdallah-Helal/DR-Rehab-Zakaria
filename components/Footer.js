cb7036d16091f827b766a21eecc51bd0d997cc5c
Abdallah Emad <108032541+Abdallah-Helal@users.noreply.github.com>
2024-05-09 19:48:30 +0300

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -10,7 +10,13 @@ export default function Footer() {
           href="https://www.linkedin.com/in/abdalla-emad-40732b1b6/"
           target="_blank"
         >
-          <span> Abdullah Emad</span>
+          <span> Abdullah Emad & </span>
+        </a>
+        <a
+          href="https://www.linkedin.com/in/ziad-elshlqany-79a29b1ba/"
+          target="_blank"
+        >
+          <span> Ziad Elshlqany </span>
         </a>
       </p>
     </div>
